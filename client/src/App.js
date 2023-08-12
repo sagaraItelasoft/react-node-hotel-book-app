66d247ff75dd41097f8b4e634b485afb505c2af5
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-08-12 10:00:18 +0530

    integrated redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
+
+/**
+ * Lets create TopNavigation/menu bar so that we can easily TopNavigate between pages
+ * lets write it in App.js before we move it to its own component
+ */
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
