3ea8c11602e4c19bda54c71734abf427731e674a
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-08-12 11:15:57 +0530

    registration page

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    alert("send user info to backend");
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      show the form <button type="submit">Submit</button>{" "}
+    </form>
+  );
+
   return (
-    <div className="container-fluid h1 p-5 text-center">Register Page</div>
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
