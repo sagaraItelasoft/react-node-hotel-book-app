97b0e397dd51438a4b07c7a9edaf0cd034b5cc63
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-08-13 20:38:46 +0530

    show hotel image completed

--- client/src/actions/hotel.js
+++ client/src/actions/hotel.js
@@ -9,3 +9,11 @@ export const createHotel = async (token, data) =>
 
 export const allHotels = async () =>
   await axios.get(`${process.env.REACT_APP_API}/hotels`);
+
+export const diffDays = (from, to) => {
+  const day = 24 * 60 * 60 * 1000;
+  const start = new Date(from);
+  const end = new Date(to);
+  const difference = Math.round(Math.abs((start - end) / day));
+  return difference;
+};
