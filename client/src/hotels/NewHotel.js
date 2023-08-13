8766915f51c4d740f1243a1ed89d1e8a2f1ca471
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-08-13 12:18:20 +0530

    hotel create form completed

--- client/src/hotels/NewHotel.js
+++ client/src/hotels/NewHotel.js
@@ -1,22 +1,20 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
-import AlgoliaPlaces from "algolia-places-react";
-import { DatePicker } from "antd";
-import moment from "moment";
+import { DatePicker, Select } from "antd";
+import { createHotel } from "../actions/hotel";
+import { useSelector } from "react-redux";
+import HotelCreateForm from "../components/forms/HotelCreateForm";
 
-const config = {
-  appId: process.env.REACT_APP_ALGOLIA_APP_ID,
-  apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
-  language: "en",
-  // countries: ["au"],
-};
+const { Option } = Select;
 
 const NewHotel = () => {
+  // redux
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { token } = auth;
   // state
   const [values, setValues] = useState({
     title: "",
     content: "",
-    location: "",
     image: "",
     price: "",
     from: "",
@@ -26,11 +24,33 @@ const NewHotel = () => {
   const [preview, setPreview] = useState(
     "https://via.placeholder.com/100x100.png?text=PREVIEW"
   );
+  const [location, setLocation] = useState("");
   // destructuring variables from state
-  const { title, content, location, image, price, from, to, bed } = values;
+  const { title, content, image, price, from, to, bed } = values;
 
-  const handleSubmit = (e) => {
-    //
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(values);
+    // console.log(location);
+
+    let hotelData = new FormData();
+    hotelData.append("title", title);
+    hotelData.append("content", content);
+    hotelData.append("location", location);
+    hotelData.append("price", price);
+    image && hotelData.append("image", image);
+    hotelData.append("from", from);
+    hotelData.append("to", to);
+    hotelData.append("bed", bed);
+
+    console.log([...hotelData]);
+
+    let res = await createHotel(token, hotelData);
+    console.log("HOTEL CREATE RES", res);
+    toast("New hotel is posted");
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   };
 
   const handleImageChange = (e) => {
@@ -43,93 +63,6 @@ const NewHotel = () => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const hotelForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label className="btn btn-outline-secondary btn-block m-2 text-left">
-          Image
-          <input
-            type="file"
-            name="image"
-            onChange={handleImageChange}
-            accept="image/*"
-            hidden
-          />
-        </label>
-
-        <input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          placeholder="Title"
-          className="form-control m-2"
-          value={title}
-        />
-
-        <textarea
-          name="content"
-          onChange={handleChange}
-          placeholder="Content"
-          className="form-control m-2"
-          value={content}
-        />
-
-        <AlgoliaPlaces
-          className="form-control m-2"
-          placeholder="Location"
-          defaultValue={location}
-          options={config}
-          onChange={({ suggestion }) =>
-            setValues({ ...values, location: suggestion.value })
-          }
-          style={{ height: "50px" }}
-        />
-
-        <input
-          type="number"
-          name="price"
-          onChange={handleChange}
-          placeholder="Price"
-          className="form-control m-2"
-          value={price}
-        />
-
-        <input
-          type="number"
-          name="bed"
-          onChange={handleChange}
-          placeholder="Number of Beds"
-          className="form-control m-2"
-          value={bed}
-        />
-      </div>
-
-      <DatePicker
-        placeholder="From date"
-        className="form-control m-2"
-        onChange={(date, dateString) =>
-          setValues({ ...values, from: dateString })
-        }
-        disabledDate={(current) =>
-          current && current.valueOf() < moment().subtract(1, "days")
-        }
-      />
-
-      <DatePicker
-        placeholder="To date"
-        className="form-control m-2"
-        onChange={(date, dateString) =>
-          setValues({ ...values, to: dateString })
-        }
-        disabledDate={(current) =>
-          current && current.valueOf() < moment().subtract(1, "days")
-        }
-      />
-
-      <button className="btn btn-outline-primary m-2">Save</button>
-    </form>
-  );
-
   return (
     <>
       <div className="container-fluid bg-secondary p-5 text-center">
@@ -139,7 +72,15 @@ const NewHotel = () => {
         <div className="row">
           <div className="col-md-10">
             <br />
-            {hotelForm()}
+            <HotelCreateForm
+              values={values}
+              setValues={setValues}
+              handleChange={handleChange}
+              handleImageChange={handleImageChange}
+              handleSubmit={handleSubmit}
+              location={location}
+              setLocation={setLocation}
+            />
           </div>
           <div className="col-md-2">
             <img
@@ -148,6 +89,7 @@ const NewHotel = () => {
               className="img img-fluid m-2"
             />
             <pre>{JSON.stringify(values, null, 4)}</pre>
+            {JSON.stringify(location)}
           </div>
         </div>
       </div>
